fa922cafef33c5405c985e479d5d76f1d8ac9e92
Kelvim Gelder <kelvingelder24@gmail.com>
2025-06-21 23:11:07 +0000

    its still kinda choppy and not as smooth

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -83,8 +83,8 @@ export default {
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        'accordion-down': 'accordion-down 0.3s ease-in-out',
+        'accordion-up': 'accordion-up 0.3s ease-in-out',
       },
     },
   },
